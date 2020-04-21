608c9ffd1b301e2224652c4c0ea7107193186049
Mike Verdicchio <mikeverdicchio@gmail.com>
2020-04-21 00:26:22 -0400

    Ping db on setup

--- cmd/toast/main.go
+++ cmd/toast/main.go
@@ -32,7 +32,12 @@ func main() {
 	logger.Info("Setting up database connection")
 	db, err := sql.Open("postgres", connStr)
 	if err != nil {
-		logger.Error("Could not connect to database")
+		logger.Error("Could not setup database")
+	}
+
+	err = db.Ping()
+	if err != nil {
+		logger.Fatal("Could not establish a connection with the database")
 	}
 
 	// Setup router and handlers
